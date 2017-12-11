d45e5f7ff9a4fc6c14187c65dbb40f2143a543e0
joerivrij <joeri@beveragerandomizer.com>
2017-12-11 20:52:22 +0100

    Added integrationtest to be inserted into the travis build (and replace the current test)

---
+++ bevrand.intregrationtest/src/test/java/com/beveragerandomizer/componenttests/randomizeApiTests.java
@@ -0,0 +1,13 @@
+package com.beveragerandomizer.componenttests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class randomizeApiTests {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
---
+++ bevrand.intregrationtest/src/test/java/com/beveragerandomizer/systemtests/SystemTest.java
@@ -0,0 +1,13 @@
+package com.beveragerandomizer.systemtests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SystemTest {
+
+    @Test
+    void myFirstTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
