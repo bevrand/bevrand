cbbf6d30b401e106840db2f2eab7265eae307805
Joost Smit <j.joostsmit@gmail.com>
2017-12-17 22:19:20 +0100

    Fixed Masthead and navbar not rendering in frontend

--- bevrand.frontend/client/src/App.js
+++ bevrand.frontend/client/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Navbar from './components/navbar';
+import Header from './components/masthead';
 import logo from './logo.svg';
 import './App.css';
 
@@ -55,11 +57,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        { renderHeader() }
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header> */}
+        <Navbar />
+        <Header />
+        {/* { renderHeader() } */}
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
