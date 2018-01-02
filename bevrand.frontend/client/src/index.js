d1d72c6786056779f61a07aa2fed4945e6e8091f
Joost Smit <j.joostsmit@gmail.com>
2018-01-02 23:37:34 +0100

    Added old javascript jquery based scripts back to the app

--- bevrand.frontend/client/src/index.js
+++ bevrand.frontend/client/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'font-awesome/css/font-awesome.css';
 // Check if magnific-popup css is needed
-import './index.css';
+// import './index.css';
+import './original-scss/combinedStyles.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('randomize-area'));
 registerServiceWorker();
