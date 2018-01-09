383f833df8913999df358de62a3fa9dbe198b8d2
Joost Smit <j.joostsmit@gmail.com>
2018-01-09 22:16:15 +0100

    Fixed order of navitems, fixed bugs on switching playlists

--- bevrand.frontend/client/src/components/randomizer.js
+++ bevrand.frontend/client/src/components/randomizer.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const PreviewItem = (props) => {
-  let amountRandomized = props.amount ? props.amount : 0;
+const PreviewItemRow = (props) => {
+  const beverage = props.value;
   return (
     <li className="list-group-item justify-content-between">
-      {props.value}
-      <span className="badge badge-default badge-pill pull-right">{amountRandomized}</span>
+      {beverage}
+      <span className="badge badge-default badge-pill pull-right">0</span>
     </li>
   );
 }
@@ -15,47 +15,103 @@ const PlaylistPreview = (props) => {
   return (
     <div>
       <ul className="list-group">
-        {listOfBeverages.map((beverage, index) => 
-          <PreviewItem key={index} value={beverage} />
+        {listOfBeverages.map((beverage, index) =>
+          <PreviewItemRow key={index} value={beverage} />
         )}
       </ul>
     </div>
   );
 }
 
+const RandomizeButton = (props) => {
+  return (
+    <div className="col-lg-8 mx-auto text-center">
+      <a id="randomize-button" className="btn btn-primary btn-xl" onClick={props.onClick} href="#getstarted">Randomize!</a>
+      <a className="btn btn-primary btn-xl js-scroll-trigger" href="#portfolio">Choose list</a>
+    </div>
+  )
+}
+
+
+/**
+ * Uses the provided Playlist to randomize a beverage
+ * @param {object} playlist 
+ */
+const randomizeBeverageMock = (playlist) => {
+  const amountOfBeverages = playlist.beverages.length;
+  const randomizedIndex = Math.floor(Math.random() * (amountOfBeverages));
+  return playlist.beverages[randomizedIndex];
+}
 
 /**
  * Randomizer, needs 1 playlist as prop input
  * @param {*} props 
  */
-const Randomizer = (props) => {
+class Randomizer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      result: null,
+      playlist: this.props.playlist
+    }
+
+    this.handleRandomize = this.handleRandomize.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps){
+    this.setState({
+      playlist: this.props.playlist
+    });
+  }
+
+  handleRandomize(){
+    //TODO:Update history of Randomized Beverages
+    
+
+    //Randomize the beverage
+    const randomizedBeverage = randomizeBeverageMock(this.props.playlist);
+    
+    //Set Result, so Component will be updated
+    this.setState({
+      result: randomizedBeverage
+    });
+  }
+
   //TODO: make the rendering based on the supplied data
   //TODO: split this component up in several smaller components
+  render() {
+    const playlist = this.props.playlist;
 
-  return (
-    <section className="bg-primary" id="getstarted">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-8 mx-auto text-center">
-            <h2 className="section-heading text-white">{props.playlist.name}</h2>
-            <hr className="light" />
+    return (
+      <section className="bg-primary" id="getstarted">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 mx-auto text-center">
+              <h2 className="section-heading text-white">{playlist.name}</h2>
+              <hr className="light" />
+            </div>
+          </div>
+          <div className="row">
+            <RandomizeButton onClick={this.handleRandomize} />
+          </div>
+          <div id="random-output" className="row">
+          {this.state.result != null &&
+              <div className="alert alert-success" role="alert">
+                You have randomized: {this.state.result}
+              </div>
+            }
+          </div>
+          <div className="row">
+            <div className="col-lg-8 mx-auto">
+              <PlaylistPreview {...playlist} />
+            </div>
           </div>
         </div>
-        <div id="random-output" className="row"></div>
-        <div className="row">
-          <div className="col-lg-8 mx-auto text-center">
-            <a id="randomize-button" className="btn btn-primary btn-xl" href="#getstarted">Randomize!</a>
-            <a className="btn btn-primary btn-xl js-scroll-trigger" href="#portfolio">Choose list</a>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-lg-8 mx-auto">
-            <PlaylistPreview {...props.playlist}/>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
+
 
 export default Randomizer;
