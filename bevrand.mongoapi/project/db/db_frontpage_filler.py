2d1aab9cc6c563dc1c8b66348c52b16fd76bc2d8
joerivrij <joeri@beveragerandomizer.com>
2018-01-15 19:53:12 +0100

    Added more data to the get method of the mongoapi

--- bevrand.mongoapi/project/db/db_frontpage_filler.py
+++ bevrand.mongoapi/project/db/db_frontpage_filler.py
@@ -2,6 +2,7 @@ from werkzeug.local import LocalProxy
 
 from project.db import db_connection
 from project.db import error_handler
+from project.api.models import ReturnModelGet
 
 from flask import jsonify, g
 
@@ -39,7 +40,12 @@ def get_frontpage_beverages(list):
             beverages.append(drinks['name'])
     except:
         return "Unknown error"
-    return beverages
+    id = str(specified_document['_id'])
+    user_name = 'frontPage'
+    list_name = list
+    image_url = specified_document['imagename']
+    front_page_model = ReturnModelGet(id, user_name, list_name, beverages, image_url)
+    return front_page_model
 
 
 def check_if_frontpage_list_exists(list):
