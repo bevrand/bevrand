1ae520802b0a85dfa45a3a6a85751ea8fcb07cc2
Joost Smit <j.joostsmit@gmail.com>
2018-01-18 22:06:50 +0100

    Removed tracking of some node_modules packages

--- bevrand.frontend/client/src/App.js
+++ bevrand.frontend/client/src/App.js
@@ -2,86 +2,6 @@ import React, { Component } from 'react';
 import Randomizer from './components/randomizer';
 import Playlists from './components/playlists';
 
-let playlistData = [
-  {
-    id: 0,
-    name: "Girls night out",
-    fullImageUrl: "img/portfolio/fullsize/Girls night out - small.jpg",
-    thumbImageUrl: "img/portfolio/thumbnails/Girls night out - small.jpg",
-    beverages: [
-      "melk",
-      "bier",
-      "wijn",
-      "fruitsapje"
-    ]
-  },
-  {
-    id: 1,
-    name: "Highland Games",
-    fullImageUrl: "img/portfolio/fullsize/Highland Games - small.jpg",
-    thumbImageUrl: "img/portfolio/thumbnails/Highland Games - small.jpg",
-    beverages: [
-      "Palm",
-      "Tripel",
-      "wijn",
-      "fruitsapje"
-    ]
-  },
-  {
-    id: 2,
-    name: "Mancave mayhem",
-    fullImageUrl: "img/portfolio/fullsize/Mancave mayhem - small.jpg",
-    thumbImageUrl: "img/portfolio/thumbnails/Mancave mayhem - small.jpg",
-    beverages: [
-      "Champagne",
-      "Rode wijn",
-      "Witte wijn"
-    ]
-  },
-  {
-    id: 3,
-    name: "Office madness",
-    fullImageUrl: "img/portfolio/fullsize/Office madness - small.jpg",
-    thumbImageUrl: "img/portfolio/thumbnails/Office madness - small.jpg",
-    beverages: [
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6"
-    ]
-  },
-  {
-    id: 4,
-    name: "Thank god it's friday",
-    fullImageUrl: "img/portfolio/fullsize/Thank god it's friday - small.jpg",
-    thumbImageUrl: "img/portfolio/thumbnails/Thank god it's friday - small.jpg",
-    beverages: [
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6"
-    ]
-  },
-  {
-    id: 5,
-    name: "Happy Holidays",
-    fullImageUrl: "img/portfolio/fullsize/The most wonderful time - small.jpg",
-    thumbImageUrl: "img/portfolio/thumbnails/The most wonderful time - small.jpg",
-    beverages: [
-      "1",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6"
-    ]
-  }
-];
-
 class App extends Component {
 
 
@@ -100,7 +20,9 @@ class App extends Component {
 
   componentDidMount(){
     this.getFrontpagePlaylists()
-      .then(res => this.setState({ playlists: res.playlists }))
+      .then(res => {
+        this.setState({ playlists: res.playlists })
+      })
       .catch(err => console.log(err));
   }
 
@@ -112,15 +34,6 @@ class App extends Component {
 
     return body;
   }
-
-  // componentWillMount(){
-  //   const retrievedPlaylists = playlistData;
-  //
-  //   // Set the current set of Playlists
-  //   this.setState({
-  //     playlists: retrievedPlaylists
-  //   });
-  // }
 
   changePlaylist(id){
     this.setState({
