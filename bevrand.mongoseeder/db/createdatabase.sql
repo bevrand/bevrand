44954271f484e7d68465944e06c67d1645167778
joerivrij <joerivrijaldenhoven@gmail.com>
2018-02-03 13:27:02 +0100

    made the authenticationapi testable on travis

--- bevrand.mongoseeder/db/createdatabase.sql
+++ bevrand.mongoseeder/db/createdatabase.sql
@@ -1,1 +1,13 @@
 CREATE DATABASE bevrand;
+\connect bevrand;
+CREATE TABLE users
+(
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(250) NOT NULL,
+    email VARCHAR(60),
+    active BOOLEAN NOT NULL,
+    datecreated TIMESTAMP,
+    dateupdated TIMESTAMP
+);
+CREATE UNIQUE INDEX users_username_uindex ON users (username);
