88f2c089cf6c88408a485fb2d8969cd272eff300
Joost Smit <joost.smit@hotmail.com>
2018-02-11 00:47:09 +0100

    Redis data now updates correctly for every randomize, change of playlist and when loading the page for the first time

---
+++ bevrand.frontend/api/controllers/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  playlists: require('./playlists'),
+  randomize: require('./randomize'),
+  redis: require('./redis')
+}
--- bevrand.frontend/api/index.js
+++ bevrand.frontend/api/index.js
@@ -4,12 +4,13 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const rp = require('request-promise');
-const Promise = require('bluebird');
 const debug = require('debug')('bevrand.frontend:api');
 
 const config = require('./config');
 
-var app = express();
+const controllers = require('./controllers');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -17,118 +18,58 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//TODO: Refactor into seperate modules
-
 /**
  * API routes
  */
-app.get('/api/frontpagelists', (req, res, next) => {
-  //TODO: Concat the results from the redis api for the default playlist
-  rp(`${config.mongoApi}/api/frontpage`)
-    .then(result => {
-      debug('Got successful result from /frontpagelists: ' + result);
-      let parsedResult = JSON.parse(result);
-      
-      return parsedResult.front_page_lists;
-    })
-    .then((result) => {
-      let promises = result.map(value => {
-        return rp(`${config.mongoApi}/api/frontpage?list=${value}`)
-          .then(response => { 
-            return JSON.parse(response); 
-          });
-      });
-
-      return Promise.all(promises);
-    })
-    .then(results => {
-      debug('Sending results', results);
-      return res.send({playlists: results});
-    })
-    .catch(err => {
-      debug('Got error from frontpagelists' + err);
-      return next(err);
-    });
-});
-
+app.get('/api/frontpagelists', 
+  controllers.playlists.getFrontpageLists(config.mongoApi, config.randomizerApi)
+);
 //TODO: add route to retrieve playlists of other users
 
-app.get('/api/redis', (req, res, next) => {
-  if(!req.query.list || !req.query.user){
+app.post('/api/redis', [(req, res, next) => {
+  //TODO: refactor query checks into seperate module
+  if (!req.query.list || !req.query.user) {
     let err = new Error('Required parameter list or user was not specified');
     debug('Error: required parameter list not specified');
     err.status = 400;
     return next(err);
+  } else {
+    return next();
   }
+}, controllers.redis.getRedis(config.randomizerApi)
+]);
 
-  rp(`${config.randomizerApi}/api/redis?user=${req.query.user}&list=${req.query.list}&topfive=false`)
-    .then(result => {
-      debug('Got succesful result from /redis: ' + result);
-      return res.send(result);
-    })
-    .catch(err => {
-      debug('Got error from randomizer api, redis route' + err);
-      return next(err);
-    });
-})
-
-app.post('/api/randomize', (req, res, next) => {
-  if(!req.query.user || !req.query.list){
+app.post('/api/randomize', [(req, res, next) => {
+  if (!req.query.user || !req.query.list) {
     let err = new Error('Required query parameters are not present');
     err.status = 400;
     return next(err);
-  }
-
-  if(!Array.isArray(req.body.beverages)){
+  } else if(!Array.isArray(req.body.beverages)){
     let err = new Error('The POST body does not contain a beverages array');
     err.status = 400;
     return next(err);
+  } else {
+    return next();
   }
-
-  const beverages = req.body.beverages;
-  const list = req.query.list;
-  const user = req.query.user;
-  debug(`api/randomize: Received parameters, playlist: ${beverages}, list: ${list}, user:${user}`);
-  
-  let randomizedBeverage;
-  rp({
-    method: 'POST',
-    uri: `${config.randomizerApi}/api/randomize?user=${user}&list=${list}`,
-    body: {
-      user: user,
-      list: list,
-      beverages: beverages
-    },
-    json: true
-  }).then(result => {
-    randomizedBeverage = result;
-    return rp(`${config.randomizerApi}/api/redis?user=${user}&list=${list}&topfive=false`);
-  }).then(result => {
-    return res.send({ 
-      result: randomizedBeverage,
-      history: JSON.parse(result)
-    });
-  }).catch(err => {
-    debug(`Error: ${err.message}`);
-    return next(err);
-  });
-});
+}, controllers.randomize.getRandomize(config.randomizerApi)
+]);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') !== 'production' ? err : {};
 
   // render the error page
   let responseStatus = err.status || 500;
+  console.error(err);
   res.status(responseStatus).send({
     result: 'error',
     message: err.message,
