79f53e7f5a926cc880bf0c28543e65001f38629e
joerivrij <joerivrijaldenhoven@gmail.com>
2018-03-10 15:09:39 +0100

    try it again

---
+++ bevrand.authenticationapi/bevrand.authenticationapi/Services/SqlUserData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using bevrand.authenticationapi.DAL;
+using bevrand.authenticationapi.DAL.Models;
+
+namespace bevrand.authenticationapi.Services
+{
+    public class SqlUserData : IUserData
+    {
+        private UserContext _context;
+        
+        public SqlUserData(UserContext context)
+        {
+            _context = context;
+        }
+        
+        public bool CheckIfUserExists(string name)
+        {
+            var sqlResult = _context.UserModel.All(u => u.UserName == name);
+            return sqlResult;
+        }
+
+        public IEnumerable<UserModel> GetAllUsers()
+        {
+            return _context.UserModel.OrderBy(u => u.Id);
+        }
+
+    }
+}
+
+
