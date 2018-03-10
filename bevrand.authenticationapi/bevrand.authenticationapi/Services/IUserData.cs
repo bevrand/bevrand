c87f9c83bc548bf8ec5a04dd1c87b29603224d0e
joerivrij <joerivrijaldenhoven@gmail.com>
2018-03-10 15:09:39 +0100

    try it again

---
+++ bevrand.authenticationapi/bevrand.authenticationapi/Services/IUserData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using bevrand.authenticationapi.DAL.Models;
+
+namespace bevrand.authenticationapi.Services
+{
+    public interface IUserData
+    {
+        bool CheckIfUserExists(string name);
+        IEnumerable<UserModel> GetAllUsers();
+    }
+}
