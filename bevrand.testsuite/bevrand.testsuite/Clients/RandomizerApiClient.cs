a1ea6f91425755e6385fc0412489908e4be7ffb6
joerivrij <joerivrijaldenhoven@gmail.com>
2018-04-02 17:35:58 +0200

    We now have 20 testcases

--- bevrand.testsuite/bevrand.testsuite/Clients/RandomizerApiClient.cs
+++ bevrand.testsuite/bevrand.testsuite/Clients/RandomizerApiClient.cs
@@ -1,4 +1,11 @@
-﻿namespace bevrand.testsuite.Clients
+﻿using System;
+using bevrand.testsuite.Models;
+using bevrand.testsuite.Models.MongoApi;
+using bevrand.testsuite.Models.RandomizerApi;
+using Flurl.Http;
+using Newtonsoft.Json;
+
+namespace bevrand.testsuite.Clients
 {
     public class RandomizerApiClient
     {
@@ -8,5 +15,33 @@
         {
             ApiUrl = url;
         }
+        
+        public BaseResponseModel PostASimpleList(RandomizePostRequest request)
+        {
+            try
+            {
+                var getStatusUrl = ApiUrl + "/api/randomize";
+                var res = getStatusUrl.PostJsonAsync(request).Result;
+                var content = res.Content.ReadAsStringAsync().Result;
+                var responseModel = new RandomizePostResult
+                {
+                    beverage = content,
+                    statusCode = (int) res.StatusCode
+                };
+
+                return responseModel;
+            }
+            catch (AggregateException e)
+            {
+                var flurlException = e.InnerException as FlurlHttpException;
+                var responseModel = new ErrorModel
+                {
+                    message = flurlException.Message,
+                    statusCode = (int) flurlException.Call.Response.StatusCode
+                };
+                return responseModel;
+            }
+        }
+
     }
 }
