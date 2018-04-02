4d39f02643703b6f285900290f092c2ed60e9054
joerivrij <joerivrijaldenhoven@gmail.com>
2018-04-02 17:36:05 +0200

    We now have 20 testcases

---
+++ bevrand.testsuite/bevrand.testsuite/TestClasses/TestSuiteAuthenticationApiTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace bevrand.testsuite.TestClasses
+{
+    [Collection("TestSuite Collection")]
+    public class TestSuiteAuthenticationApiTests
+    {
+        private readonly TestSuiteFixture _fixture;
+        
+        public TestSuiteAuthenticationApiTests(TestSuiteFixture _fixture)
+        {
+            this._fixture = _fixture;
+        }
+    }
+}
