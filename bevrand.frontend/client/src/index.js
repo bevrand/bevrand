2592818f22a7ee88616eb6019850306ca346ccfe
Joost Smit <joost.smit@hotmail.com>
2018-04-03 10:48:22 +0200

    Frontend - started to add to react-router

--- bevrand.frontend/client/src/index.js
+++ bevrand.frontend/client/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 //TODO: add other javascript to this bundler
 
 import './original-scss/combinedStyles.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('randomize-area'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>, document.getElementById('randomize-area'));
 registerServiceWorker();
