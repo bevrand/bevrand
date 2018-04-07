7748c9c0cfc16e376bc07965db421003c071fac3
Joost Smit <joost.smit@hotmail.com>
2018-04-07 14:47:50 +0200

    Splitted the Header and navigation into seperate react components

--- bevrand.frontend/client/src/App.js
+++ bevrand.frontend/client/src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Header from './components/Header';
 import Randomizer from './components/Randomizer';
 import Playlists from './components/Playlists';
 import TopRolledBeverages from './components/TopRolledBeverages';
@@ -63,10 +65,18 @@ class App extends Component {
     }
     return (
       <div className="App">
-        {/* TODO: add routing from react-router to the app component */}
+        <Navigation />
+        <Header />
+
+      {/* TODO: put these components in a seperate Component, like this:
+        https://medium.com/@pshrmn/a-simple-react-router-v4-tutorial-7f23ff27adf */}
+      {/* Passing the props (of the main logged in user to the components like so:
+        https://github.com/ReactTraining/react-router/issues/5521) */}
         <Randomizer playlist={this.state.currentPlaylist} />
         <Playlists playlists={this.state.playlists} onClick={this.changePlaylist} />
         <TopRolledBeverages playlist={this.state.currentPlaylist} />
+
+      {/* TODO: Put the components below in a Main component */}
         <Login />
         <SignUp />
       </div>
--- bevrand.frontend/client/src/index.js
+++ bevrand.frontend/client/src/index.js
@@ -7,8 +7,9 @@ import './original-scss/combinedStyles.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
+ReactDOM.render((
   <BrowserRouter>
-    <App />
-  </BrowserRouter>, document.getElementById('randomize-area'));
+      <App />
+  </BrowserRouter>
+  ), document.getElementById('root'));
 registerServiceWorker();
