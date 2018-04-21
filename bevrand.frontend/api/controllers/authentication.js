8ec6d5877325ddbb39ce73cc3d9d9d1308aab44a
Joost Smit <joost.smit@hotmail.com>
2018-04-21 13:39:06 +0200

    Bugfixes in loggin in and registering, register now uses same css as login, logout is nog working

--- bevrand.frontend/api/controllers/authentication.js
+++ bevrand.frontend/api/controllers/authentication.js
@@ -4,10 +4,10 @@ const debug = require('debug')('controllers:authentication');
 
 const handleLogin = (url, secret, expirationTime) => {
   return (req, res, next) => {
-    const { username, passWord } = req.body;
+    const { userName, passWord, emailAddress } = req.body;
     
     //TODO: If email address is provided, we first need to retrieve the username and id
-    if(!username || !passWord) {
+    if(!(userName || emailAddress) || !passWord) {
       let err = new Error('Required body elements are not present');
       err.status = 400;
       return next(err);
@@ -30,7 +30,7 @@ const handleLogin = (url, secret, expirationTime) => {
       } else {
         return rp({
           method: 'GET',
-          uri: `${url}/api/Users?username=${username}`
+          uri: `${url}/api/Users/by-username/${userName}`
         });
       }
     }).then((result) => {
@@ -43,7 +43,7 @@ const handleLogin = (url, secret, expirationTime) => {
         });
       }
       // Is authenitcationExpirationTime a number?
-      let token = jwt.sign({ id:id, username: username }, secret, {expiresIn: expirationTime})
+      let token = jwt.sign({ id:id, username: userName }, secret, {expiresIn: expirationTime})
       return res.send({
         success: true,
         message: 'Succesfully authenticated User',
@@ -57,21 +57,19 @@ const handleLogin = (url, secret, expirationTime) => {
 }
 
 const registerUser = (authenticationUrl) => {
-  return (req, res, next) => {  
-    if(!req.body.username || !req.body.passWord || !req.body.emailAddress) {
+  return (req, res, next) => {
+    const { userName, passWord, emailAddress } = req.body;
+    
+    if(!userName || !passWord || !emailAddress) {
       let err = new Error('Required body elements are not present');
       err.status = 400;
       return next(err);
     }
+
     rp({
       method: 'POST',
       uri: `${authenticationUrl}/api/Users`,
-      body: {
-        username: req.body.username,
-        emailAddress: req.body.emailAddress,
-        passWord: req.body.passWord,
-        active: true
-      },
+      body: req.body,
       json: true
     }).then(result => {
       return res.send(result);
