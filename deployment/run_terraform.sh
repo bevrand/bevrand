a491cf0345931f94ba009525e5306cae6274640d
Collin Kleine <collin.kleine@gmail.com>
2018-06-10 21:00:59 +0200

    https://trello.com/c/T6Su5C0e Taint tls_private_key as well to always generate a new key. Now generates TLS SSH key, adds it to digital ocean, and blocks itself after running it's commands. Developers should now ssh using "developer".

--- deployment/run_terraform.sh
+++ deployment/run_terraform.sh
@@ -2,6 +2,7 @@
 
 
 ../terraform init -backend-config="storage_account_name=$storage_account_name" -backend-config="access_key=$access_key"
+../terraform taint tls_private_key.terraformusersshkey
 ../terraform taint digitalocean_droplet.docker
 ../terraform plan -out outfile
 ../terraform apply -auto-approve outfile
