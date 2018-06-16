9a2e6f9ac8253519a4a73f9decb8a49fdbfd168f
joerivrij <joerivrijaldenhoven@gmail.com>
2018-06-16 18:29:24 +0200

    First commit of cypress for ui testing

--- demo_push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "starting"
-$(docker tag bevrand_nodefrontend bevrand/reactfrontend:develop)
-echo "pushing"
-echo $(docker push bevrand/reactfrontend:develop)
-echo "Done pushing"
