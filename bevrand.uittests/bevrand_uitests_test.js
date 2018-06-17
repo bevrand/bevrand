2fc0c892843d3877147ece14d5660de3b782a601
joerivrij <joerivrijaldenhoven@gmail.com>
2018-06-17 15:54:29 +0200

    ui tests with cypress run 1

--- bevrand.uittests/bevrand_uitests_test.js
+++ bevrand.uittests/bevrand_uitests_test.js
@@ -1,21 +1,23 @@
 let userName = '';
 let passWord = '';
 
-describe('Page comes up', function () {
-    it('.Visits the beverage randomizer', function () {
+context('Beverage Randomizer Test', () => {
+    beforeEach(() => {
         cy.visit('http://0.0.0.0:4540')
+    })
+
+describe('Page comes up', function () {
+    it('Visits the beverage randomizer', function () {
         cy.title().should('eq', 'The Beverage Randomizer')
     })})
 
 describe('Randomize functionality works', function () {
-    it('.should be able to randomize my drinks', function () {
-        cy.visit('http://0.0.0.0:4540')
+    it('Should be able to randomize my drinks', function () {
         cy.get('#letsGetStartedButton').click()
         cy.get('#randomizeButton').click()
         cy.get('#randomizedOutput').contains('randomized')
     })
-    it('.should be able to switch list', function () {
-        cy.visit('http://0.0.0.0:4540')
+    it('Should be able to switch list', function () {
         cy.get('#letsGetStartedButton').click()
         cy.get("#currentlySelectedPlaylist").then(($currentList) => {
             const firstList = $currentList.text()
@@ -23,41 +25,76 @@ describe('Randomize functionality works', function () {
             cy.get('#playlists > div > div > div:nth-child(2) > a > div').click()
             cy.get("#currentlySelectedPlaylist").then(($secondList) => {
                 expect($secondList.text()).not.to.eq(firstList)
+            })
         })
     })
-    })
-    it('.The randomized drink is in the list', function () {
-        cy.visit('http://0.0.0.0:4540')
+    it('The randomized drink is in the list', function () {
         cy.get('#letsGetStartedButton').click()
         cy.get('#randomizeButton').click()
         cy.get('#randomizedOutput').then(($randomizedDrink) => {
-            let firstDrink = $randomizedDrink.text().split(':')[1];
-            console.log(firstDrink)
+            let firstDrink = $randomizedDrink.text().split(':')[1].split(' ')[1];
+            cy.get('#getstarted > div > div:nth-child(4) > div > div > ul').contains(firstDrink)
+        })
+    })
+    it('The randomized drink is in the list after switch', function () {
+        cy.get('#topChooseListButton').click()
+        cy.get('#playlists > div > div > div:nth-child(4) > a > div').click()
+        cy.get('#randomizeButton').click()
+        cy.get('#randomizedOutput').then(($randomizedDrink) => {
+            let firstDrink = $randomizedDrink.text().split(':')[1].split(' ')[1];
+            cy.get('#getstarted > div > div:nth-child(4) > div > div > ul').contains(firstDrink)
         })
     })
 })
 
-// describe('Register', function () {
-//     it('.should be able to reach the register page', function () {
-//         cy.visit('http://0.0.0.0:4540')
-//         cy.get('#navbarResponsive > ul > li:nth-child(2) > a').click()
-//         cy.url().should('eq', 'http://0.0.0.0:4540/register')
-//     })
+describe('Top rolled beverages works', function () {
+    it('Buttons should be clickable', function () {
+        cy.get('#topFiveLinkButton').click()
+    })
+    it('Current list should match top five list', function () {
+        cy.get('#letsGetStartedButton').click()
+        cy.get("#currentlySelectedPlaylist").then(($currentList) => {
+            const currentList = $currentList.text()
+            cy.get('#topFiveLinkButton').click()
+            cy.get("#currentlySelectedPlaylist").then(($topFiveList) => {
+                expect($topFiveList.text()).eq(currentList)
+            })
+        })
+    })
+    it('Switching list should switch topfive', function () {
+        cy.get('#letsGetStartedButton').click()
+        cy.get("#currentlySelectedPlaylist").then(($currentList) => {
+            const currentList = $currentList.text()
+            cy.get('#playlists > div > div > div:nth-child(4) > a > div').click()
+            cy.get('#topFiveLinkButton').click()
+            cy.get("#currentlySelectedPlaylist").then(($topFiveList) => {
+                expect($topFiveList.text()).not.to.eq(currentList)
+            })
+        })
+    })
+})
 
-//     it('.should be able to register a user', function () {
-//         userName = makeRandomString(15)
-//         passWord = makeRandomString(10)
+describe('Register', function () {
+    it('.should be able to reach the register page', function () {
+        cy.visit('http://0.0.0.0:4540')
+        cy.get('#navbarResponsive > ul > li:nth-child(2) > a').click()
+        cy.url().should('eq', 'http://0.0.0.0:4540/register')
+    })
 
-//         cy.get('#userName')
-//         .type(userName).should('have.value', userName)
-//         cy.get('#emailAddress')
-//         .type(`${userName}@email.com`).should('have.value', `${userName}@email.com`)
-//         cy.get('#passWord')
-//         .type(passWord).should('have.value', passWord)
-//         cy.get('#controlPassWord')
-//         .type(passWord).should('have.value', passWord)
-//     })
-// })
+    it('.should be able to register a user', function () {
+        userName = makeRandomString(15)
+        passWord = makeRandomString(10)
+
+        cy.get('#userName')
+        .type(userName).should('have.value', userName)
+        cy.get('#emailAddress')
+        .type(`${userName}@email.com`).should('have.value', `${userName}@email.com`)
+        cy.get('#passWord')
+        .type(passWord).should('have.value', passWord)
+        cy.get('#controlPassWord')
+        .type(passWord).should('have.value', passWord)
+    })
+})
 
 function makeRandomString(numberOfChars) {
     var text = ""
@@ -67,3 +104,4 @@ function makeRandomString(numberOfChars) {
         text += possible.charAt(Math.floor(Math.random() * possible.length));
     return text;
 }
+})
