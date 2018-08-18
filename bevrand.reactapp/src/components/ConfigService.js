e38951cb5853586e3a7288e40ecf83bceb2ef8e7
Joost Smit <joost.smit@hotmail.com>
2018-08-18 21:08:30 +0200

    Edited config.yml until reactapp is fixed with proxyhostname

--- bevrand.reactapp/src/components/ConfigService.js
+++ bevrand.reactapp/src/components/ConfigService.js
@@ -1,13 +1,6 @@
-const sanitizeUrl = (url) => {
-  let newUrl;
-  newUrl = url.startsWith('http://') ? url : 'http://' + url;
-  newUrl = url.endsWith('/') ? url.substring(0, url.length - 1) : url;
-  return newUrl;
-}
-
-
 const Config = {
-  proxyHostname: sanitizeUrl(process.env.REACT_APP_PROXY_HOSTNAME || 'http://localhost:4540'),
+  // proxyHostname: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:4540'
+  proxyHostname: ''
 }
 
 export default Config;
