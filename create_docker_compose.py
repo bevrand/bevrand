787902d689e59edaafb4d03fea5c343ef5814811
joerivrij <joerivrijaldenhoven@gmail.com>
2019-02-12 12:07:42 +0100

    mistakes in docker creation fix

--- create_docker_compose.py
+++ create_docker_compose.py
@@ -140,8 +140,9 @@ yaml_file = {}
 service_yaml_file = {}
 databases_services = ['dockergres', 'dockermongo', 'redis', 'neo4j']
 test_services = ['componenttest']
-api_services = ['authenticationapi', 'highscoreapi', 'randomizerapi', 'recommandationapi', 'playlistapi', 'proxyapi', 'frontendapi',
 systemtest_services = ['systemtest']
+api_services = ['authenticationapi', 'highscoreapi', 'randomizerapi', 'recommandationapi', 'playlistapi', 
+                'proxyapi', 'frontendapi', 'dockernginx']
 data_seeder_service = ['dataseeder']
 password_services = {'authenticationapi': 'dockergres', 'playlistapi': 'dockermongo'}
 volume_services = ['dockermongo', 'dockergres']
